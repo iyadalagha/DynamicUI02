691c1f1f763582a0f6612984274d4415688ddb88
iyadalagha <iyadalagha@hotmail.com>
2016-03-11 01:19:44 +0200

    sdfsd

--- app/src/main/java/com/example/probook/dynamicui02/MainActivity.java
+++ app/src/main/java/com/example/probook/dynamicui02/MainActivity.java
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Toolbar toolbar2 = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar2);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
